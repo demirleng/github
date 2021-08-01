e96fb7a5acee31460cc6857ea326845b1fa661ac
Adil Demirleng <demirleng@gmail.com>
2021-08-01 15:43:26 -0400

    third commit

---
+++ src/package1/ClassOne.java
@@ -0,0 +1,15 @@
+package package1;
+
+public class ClassOne {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("Batch 35/36 learn Github");
+        System.out.println("we like Git but we will like more when we have more practice");
+
+
+    }
+
+
+
+
+}
