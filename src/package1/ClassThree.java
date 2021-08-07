266b534a5b192e3b2079528ec71361d228b6852f
Adil Demirleng <demirleng@gmail.com>
2021-08-07 14:25:33 -0400

    new codes are pushed

---
+++ src/package1/ClassThree.java
@@ -0,0 +1,13 @@
+package package1;
+
+import java.sql.SQLOutput;
+
+public class ClassThree {
+
+    public static void main(String[] args) {
+        int x=5;
+        int sum = x+2;
+
+        System.out.println(sum);
+    }
+}
